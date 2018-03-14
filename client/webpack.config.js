f005bf23f8562ca930b73c38cbf6bc8fe11e6374
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-14 16:27:38 -0700

    [node] applied es6 support to node

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -14,7 +14,12 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['react', 'es2015', 'stage-0'],
+          presets: [
+            'react',
+            [
+              'es2015', { 'modules': false }
+            ]
+          ],
         }
       },
       {
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,3 +1,5 @@
+import express from 'express';
+import parser from 'body-parser';
 import path from 'path'
 
 const middleware = [
