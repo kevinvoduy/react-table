2b0f362f4a1722833811aa875c682f71b9dc5b6f
Kevin Vo <kevinvoduy218@gmail.com>
2018-03-15 01:18:38 -0700

    [table] left column is fixed and can scroll in x and y

--- client/src/Table/Table.jsx
+++ client/src/Table/Table.jsx
@@ -1,33 +1,82 @@
 import React, { Component } from 'react'
-import { Table, Column, Cell } from 'fixed-data-table-2';
+import { Table, Column, Cell } from 'fixed-data-table-2'
+import './table.css'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
+
   render() {
+    console.log(this.props.metrics)
     return(
       <div>
-        <Table>
-          rowsCount={this.props.object.length}
-          rowHeight={50}
-          headerHeight={50}
-          width={1000}
-          maxHeight={500}
-        >
+        <table>
+          <tr>
+            <th id='fixed'>id</th>
+            {
+              this.props.metrics.column_names.map(header => {
+                return(
+                  <th>{header}</th>
+                )
+              })
+            }
+          </tr>
           {
-            this.props.metrics.rows.map(col => {
+            this.props.object.map(item => {
               return(
-                <Column
-                  header={<Cell>col</Cell>}
-                  cell='{
-                    hello
-                  }'
-                />
+                <tr>
+                  <th id='fixed'>
+                   {item.name}
+                  </th>
+                  {
+                    this.props.metrics.column_names.map(header => {
+                      return(
+                        <th>{this.props.metrics.rows[0][header]}</th>
+                      )
+                    })
+                  }
+                </tr>
               )
             })
           }
-        </Table>
+          {
+            this.props.object.map(item => {
+              return(
+                <tr>
+                  <th id='fixed'>
+                   {item.name}
+                  </th>
+                  {
+                    this.props.metrics.column_names.map(header => {
+                      return(
+                        <th>{this.props.metrics.rows[0][header]}</th>
+                      )
+                    })
+                  }
+                </tr>
+              )
+            })
+          }
+          {
+            this.props.object.map(item => {
+              return(
+                <tr>
+                  <th id='fixed'>
+                   {item.name}
+                  </th>
+                  {
+                    this.props.metrics.column_names.map(header => {
+                      return(
+                        <th>{this.props.metrics.rows[0][header]}</th>
+                      )
+                    })
+                  }
+                </tr>
+              )
+            })
+          }
+        </table>
       </div>
     )
   }
